5f4e58ce45e5e334e90ab0fb6bf93e42e42a41e4
yasinaminuddin <89342259+yasinaminuddin@users.noreply.github.com>
2023-08-16 10:54:48 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// toggle
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const menu = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
